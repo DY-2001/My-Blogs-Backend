d01f13863bb410f548e06013af45a9c982f630a3
Dushyant Yadav <89105143+DY-2001@users.noreply.github.com>
2024-01-18 02:49:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { resourceUsage } = require("process");
 const salt = bcrypt.genSaltSync(10);
 const secret = "asdfe45we45w345wegw345werjktjwertkj";
 // { credentials: true, origin: "https://my-blogs-frontend.vercel.app" }
-app.use(cors());
+app.use(cors({credentials: true, origin: "https://my-blogs-frontend.vercel.app/"}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
