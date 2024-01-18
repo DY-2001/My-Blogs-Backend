005af0db079e211a5c21ec3e526108cf51b87af1
Dushyant Yadav <89105143+DY-2001@users.noreply.github.com>
2024-01-18 17:11:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 const multer = require("multer");
 const fs = require("fs");
 // app.use(cors());
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({credentials: true, origin: 'https://my-blogs-frontend.vercel.app'}));
 const uploadMiddleware = multer({ dest: "uploads/" });
 const { resourceUsage } = require("process");
 
